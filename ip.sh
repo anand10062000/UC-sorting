d89927d2d587ad4c70eb15fda256ec01973e39a3
anand10062000 <anandkumarramisetti@gmail.com>
2021-08-22 13:02:09 +0530

    storing data in dictionary

--- ip.sh
+++ ip.sh
@@ -2,11 +2,19 @@
 read -p :" a: " a
 read -p :" b: " b
 read -p :" c: " c
-	d=$(( $a+$b*$c ))
+   d=$(( $a+$b*$c ))
    echo $d
-	e=$(( $a*$b+$c ))
-	echo $e
-	f=$(( $c+$a/$b ))
-	echo $f
-	g=$(( $a%$b+$c ))
-	echo $g
+   e=$(( $a*$b+$c ))
+   echo $e
+   f=$(( $c+$a/$b ))
+   echo $f
+   g=$(( $a%$b+$c ))
+   echo $g
+      declare -A sort
+   sort[a+c*c]=$d
+   sort[a*b+c]=$e
+   sort[c+a/b]=$f
+   sort[a%b+c]=$g
+
+echo ${sort[*]}
+
